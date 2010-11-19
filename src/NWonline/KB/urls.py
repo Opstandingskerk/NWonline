b4bc5fd38d51d5116bdad6af04e1aae834e516d5
Jeroen <Jeroen@AFTERBUN>
2010-11-19 16:32:13 +0100

    made the MEDIA path relative

--- src/NWonline/KB/urls.py
+++ src/NWonline/KB/urls.py
@@ -18,5 +18,5 @@ urlpatterns = patterns('',
     (r'^query/persoon/.*$', 'NWonline.KB.ajax.queryPersoon'),
     
     (r'^media/(?P<path>.*)$', 'django.views.static.serve',
-        {'document_root': 'D:/Development/Projects/NWonline/media/KB'}),
+        {'document_root': '../../media/KB'}),
 )
