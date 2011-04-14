da647efffc70f78084251d679afaeb1d9251ec70
Lukas Batteau <l.batteau@indieit.nl>
2011-04-14 16:54:22 +0200

    Added wizard membership

--- src/NWonline/KB/wizards/urls.py
+++ src/NWonline/KB/wizards/urls.py
@@ -1,7 +1,9 @@
-from NWonline.KB.wizards.marry import MarryWizard, MarryForm1, MarryForm2, \
-    MarryForm3, MarryForm4
+from NWonline.KB.wizards.marry import MarryWizard, MarryForm1, MarryForm2, MarryForm3, MarryForm4
+from NWonline.KB.wizards.membership import MembershipWizard, MembershipForm1
 from django.conf.urls.defaults import patterns
 
 urlpatterns = patterns('',
     (r'^marry/(?P<persoonId>\d*)/$', MarryWizard([MarryForm1, MarryForm2, MarryForm3, MarryForm4])),
+    (r'^membership/gezin/(?P<gezinId>\d*)/$', MembershipWizard([MembershipForm1]), { 'updateWholeFamily': True }),
+    (r'^membership/(?P<persoonId>\d*)/$', MembershipWizard([MembershipForm1]), { 'updateWholeFamily': False }),
 )
